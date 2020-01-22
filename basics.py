a854642338d87ffd252cdf87700d6d964f02179a
sharvariathalye <sharvari.athalye@duke.edu>
2020-01-21 22:55:28 -0500

    Corrected script to match what was done in class

--- basics.py
+++ basics.py
@@ -1,23 +1,3 @@
-# calculator.py
-def add(x,y)
-z = x + y
-print("{} + {} = {}". format(x, y, z))
-return z
-
-if x == "a":
-    d = add (1,2)
-    if d > 100:
-        print("This number is too high.") 
-    print("{} + {} = {}". format(a, b, c))
-elif x == "s":
-        a = 20
-        b = -3
-        c = a - b
-        print("{} - {} = {}". format(a, b, c))
-
-
-
-
 # basic python
 x = input ("Enter a letter: ")
 print("You entered {}". format(x))
