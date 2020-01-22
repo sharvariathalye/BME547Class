521c530063738d60d9ec4e26577fedb3c234c87a
sharvariathalye <sharvari.athalye@duke.edu>
2020-01-21 22:37:55 -0500

    First commit

---
+++ basics.py
@@ -0,0 +1,43 @@
+# calculator.py
+def add(x,y)
+z = x + y
+print("{} + {} = {}". format(x, y, z))
+return z
+
+if x == "a":
+    d = add (1,2)
+    if d > 100:
+        print("This number is too high.") 
+    print("{} + {} = {}". format(a, b, c))
+elif x == "s":
+        a = 20
+        b = -3
+        c = a - b
+        print("{} - {} = {}". format(a, b, c))
+
+
+
+
+# basic python
+x = input ("Enter a letter: ")
+print("You entered {}". format(x))
+if x == "a":
+    a = 1
+    b = 2
+    c = a + b
+    print("{} + {} = {}". format(a, b, c))
+elif x == "s":
+        a = 20
+        b = -3
+        c = a - b
+        print("{} - {} = {}". format(a, b, c))
+elif x == "m":
+        a = 7
+        b = 9
+        c = a * b
+        print("{} * {} = {}". format(a, b, c))
+else:
+    print("The {} command is not recognized.". format(x))
+          
+print("Done") 
+    
